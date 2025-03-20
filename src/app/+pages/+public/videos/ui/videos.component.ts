ac8a56abf6246ff0075b9a9907dfb6c6fc700194
Alireza <alirezatorkaman.kaman@gmail.com>
2025-03-21 01:46:58 +0330

    fix videos link

--- src/app/+pages/+public/videos/ui/videos.component.ts
+++ src/app/+pages/+public/videos/ui/videos.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class VideosComponent {
   videos = [
-    { url: '/videos/Computer-Basic.mp4', title: 'معرفی قطعات سیستم' },
-    { url: '/videos/Graphic-Card.mp4', title: 'معرفی گرافیک های سری 50' }
+    { url: 'https://s33.picofile.com/d/8483496484/de6af24d-8da1-4325-83d4-849cb59a4051/Computer_Basic.mp4', title: 'معرفی قطعات سیستم' },
+    { url: 'https://s33.picofile.com/d/8483496476/6fc93b33-d439-4af7-9882-cf984c27df18/Graphic_Card.mp4', title: 'معرفی گرافیک های سری 50' }
   ];
 }
